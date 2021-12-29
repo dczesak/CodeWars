b0b0fe552be5ebf407ba40a9c2de5a04456cd54c
Dominik Czesak <czesak.dominik99@gmail.com>
2021-12-29 13:55:46 +0100

    Added DigPow challenge

--- src/com/czesak/Main.java
+++ src/com/czesak/Main.java
@@ -4,7 +4,7 @@ package com.czesak;
 public class Main {
 
     public static void main(String[] args) {
-        String str ="xxooo";
+      /*  String str ="xxooo";
         System.out.println(check(str));
         System.out.println("***********");
         System.out.println(validatePin("1234"));
@@ -12,6 +12,11 @@ public class Main {
         System.out.println(validatePin("1234a"));
         System.out.println(validatePin("a234"));
         System.out.println(validatePin(".234"));
+*/
+        System.out.println(DigPow.digPow(89, 1));
+        System.out.println(DigPow.digPow(92, 1));
+        System.out.println(DigPow.digPow(695, 2));
+        System.out.println(DigPow.digPow(46288, 3));
 
     }
 
@@ -19,11 +24,11 @@ public class Main {
         int x = 0;
         int o = 0;
 
-        for (int i = 0; i < str.length();i++){
-            if(str.charAt(i) == 'o' || str.charAt(i) == 'O') {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'o' || str.charAt(i) == 'O') {
                 o++;
             }
-            if(str.charAt(i) == 'x' || str.charAt(i) == 'X') {
+            if (str.charAt(i) == 'x' || str.charAt(i) == 'X') {
                 x++;
             }
         }
@@ -32,9 +37,9 @@ public class Main {
         } else return false;
     }
 
-   private static boolean validatePin(String pin){
+    private static boolean validatePin(String pin) {
         if ((pin.length() == 4 || pin.length() == 6) && pin.matches("[0-9]+")) return true;
         return false;
-   }
+    }
 }
 
