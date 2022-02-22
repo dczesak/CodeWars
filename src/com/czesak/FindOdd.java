e85eb7a3a601f170283a705e5d33d226a5960d03
Dominik <czesak.dominik99@gmail.com>
2022-02-22 17:21:03 +0100

    Add descripion to FindOdd challange

--- src/com/czesak/FindOdd.java
+++ src/com/czesak/FindOdd.java
@@ -1,6 +1,9 @@
 package com.czesak;
 
 public class FindOdd {
+    /*
+    Find integer in array that contains odd times
+     */
     public static int findIt(int[] a) {
         int odd = 0;
         for (int j : a) {
